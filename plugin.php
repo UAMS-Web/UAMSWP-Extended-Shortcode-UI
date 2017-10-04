c6630518f5359580403721610e65eb99d6410f77
todd-uams <todd@uams.edu>
2017-10-04 10:52:43 -0500

    Requirement Check (Shortcode UI & Shortcake Bakery)

--- plugin.php
+++ plugin.php
@@ -18,6 +18,10 @@ if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
 	add_action( 'admin_notices', create_function( '',
 	"echo '<div class=\"error\"><p>" . __( 'UAMSWP Extended Shortcode UI requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'uamswp-extended-shortcode-ui' ) . "</p></div>';" ) );
 	return;
+} elseif ( ! is_plugin_active( 'shortcode-ui/shortcode-ui.php') || ! is_plugin_active( 'shortcake-bakery/shortcake-bakery.php' ) ) {
+	add_action( 'admin_notices', create_function( '',
+	"echo '<div class=\"error\"><p>" . __( 'UAMSWP Extended Shortcode UI requires <a href="https://wordpress.org/plugins/shortcode-ui/">Shortcake (Shortcode UI)</a> &amp; <a href="https://wordpress.org/plugins/shortcake-bakery/">Shortcake Bakery</a> plugins to be active!', 'uamswp-extended-shortcode-ui' ) . "</p></div>';" ) );
+	return;
 } else {
 	include_once __DIR__ . '/includes/shortcode-ui.php';
 }
